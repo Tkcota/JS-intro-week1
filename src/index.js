7f4f6d1fa937ef571d3e90a621caafee65664e10
Tyler K <64233940+Tkcota@users.noreply.github.com>
2022-05-17 10:01:29 -0600

    testing first git push

--- src/index.js
+++ src/index.js
@@ -1,5 +1,349 @@
-export const TEST_STRING = "test string";
+//  1. What is the Difference Between =, ==, and === in JavaScript 
+// = is assigning value,
+// == is checking value while desregarding datatypes
+// === is checking value and datatype.
 
-export function testFunction() {
-  return TEST_STRING;
-}
+
+
+// 2. What are template literals in js?
+// `string text`
+
+// `string text line 1
+//  string text line 2`
+
+// `string text ${expression} string text`
+
+// Re-usable template:
+// const templateFn = expression => `string text ${expression} string text`;
+
+// Tagged, this calls the function "example" with the template as the
+// first argument and substitution values as subsequent arguments:
+// example`string text ${expression} string text`
+
+
+// 3. What are the differences between variables created using let, var or const?
+
+// let is an object that you create
+
+
+
+// 4. What are data types in javascript?
+
+
+
+// 5. What is difference between null vs undefined?
+
+
+
+// 6. What are various operators supported by javascript?
+
+
+
+// 7. What is 'typeof' operator?
+
+
+
+//8. what are some of the important features of JavaScript ES6
+
+
+
+//9. how does the ternary operator in javascript work?
+
+
+
+//10. what are valid variable names in js
+
+
+
+//11. what is variable scope?
+
+//11. what is variable assignment?
+
+
+
+// NUMBERS
+
+// 1. How do you generate random integers in js?
+
+
+
+// 2. What is isNaN?
+
+
+
+// 3. How to do you see if a number is even?
+
+
+
+// 4. how do you convert the string '1' to the number 1
+
+
+
+// STRINGS
+
+
+
+//1. Explain these methods
+
+// startsWith()
+
+
+
+// endsWith()
+
+
+
+// includes()
+
+
+
+// slice()
+
+
+
+// toUpperCase()
+
+
+
+// toLowerCase()
+
+
+
+// charAt()
+
+
+
+// split()
+
+
+
+// replace()
+
+
+
+CONDTIONALS
+
+
+
+
+//1. how does the if statement work in js
+
+
+//2. how does the if/else statement work in js
+
+
+//3. how does the if/elseif/else statement work in js
+
+
+//4. what is a switch statement
+
+
+//5. how is a switch statement different from if/elseif statement
+
+
+//6. what are the Comparison Operators that js uses?
+
+
+//7. what are the Logical Operators that js uses?
+
+
+//8. what are truthy and falsey values (more high level idea)
+
+
+//9. what are the specific falsey values in js
+
+
+//10. what are the specific truthy values in js(answer: anything that is not falsey)
+
+
+//11. how does a nested if statement in js work
+
+
+
+LOOPS
+
+
+
+
+//1. what is for loop in javascript
+
+
+//1a. what is the syntax for a for loop in javascript
+
+
+//2. what is while loop in javascript
+
+
+//2a. what is the syntax for a while loop in javascript
+
+
+//3. what is while for...in in javascript
+
+
+//3a. what is the syntax for a while for...in in javascript
+
+
+//4. what is nested loop, give an example?
+
+
+
+
+
+FUNCTIONS
+
+
+
+//1. how do you define a function in javascript
+
+//- how do define an arrow function
+
+//- how do you define an regular function
+
+//- what is the difference between arrow/regular functions
+
+
+//2 how do you call a function in javascript (keep this simple)
+
+
+//3 what are default parameters in a function?
+
+
+//4 what is the difference between params and arguments in js
+
+
+//5 what does the return keyword do in a function in js
+
+
+//6 what will a function return if there is no return keyword
+
+
+//7 What is an anonymous function?
+
+
+//8 What is a typical use case for anonymous functions?
+
+
+//9 Can you define a function as a parameter as function?(answer:yes)
+
+
+//10 What does it mean that you Can you define a function as a parameter as function?
+
+
+//11 Can you have a function return a function in js?(answer:yes)
+
+
+//12 What does it mean that you Can you have a function return a function in js?
+
+
+// in realation to 9-12, what is a first class function?
+
+
+
+
+ARRAYS
+
+
+
+// 1. what is an array in javascript and at a high level how does it work?
+
+
+
+// 2. Explain array methods 
+
+// pop(), 
+
+// length(), 
+
+// push(), 
+
+// shift(), 
+
+// slice(), 
+
+// splice(), 
+
+// sort(), 
+
+// unshift(), 
+
+// concat(),
+
+// map(), 
+
+// filter(), 
+
+// indexOf(), 
+
+// find(), 
+
+// findIndex(), 
+
+// includes()
+
+
+
+
+//3. What are the benefits of using spread ie'...' syntax is js (hint cloning)
+
+
+
+//4. What is array destructuring in js
+
+
+
+//5. When should you use reduce(), map(), foreach() and filter() in js?
+
+
+
+
+
+
+OBJECTS
+
+
+
+// 1. What is a object in JS?
+
+// (This can get a little complicate so this is the good overview of the basics. 
+
+//https://www.javascripttutorial.net/javascript-objects/)
+
+
+
+
+// 2. How do access a value in a js object using dot '.' notation?
+
+
+// 3. How do access a value in a js object using array like '[""]'notation?
+
+
+
+// 4. what is meant by a key value pair in an object?
+
+
+// 5. How do you add a key value pair in an object?
+
+
+// 5b. How do you delete a key value pair in an object?
+
+
+// 6. How do modify a value in an object?
+
+
+// 7. how do you see if a key is in a object?
+
+
+// 8. how can you clone an object?
+
+
+// 9. what is object destructoring?
+
+
+
+
+
+
+
+
+
+
